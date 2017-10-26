a4a7fa5996a102a2c6ee0489482ec2d0cfdae967
yuzhangbit <yu.zhang.bit@gmail.com>
2017-10-26 14:18:50 +0800

    refine the script

--- scripts/update_bit_source_list.bash
+++ scripts/update_bit_source_list.bash
@@ -23,7 +23,7 @@ update_sourceslist()
 cd /etc/apt
 sudo mv sources.list sources.list.bak
 sudo touch sources.list
-echo "deb http://mirror.bit.edu.cn/ubuntu/ xenial main restricted universe multiverse
+sudo sh -c "echo 'deb http://mirror.bit.edu.cn/ubuntu/ xenial main restricted universe multiverse
 deb http://mirror.bit.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
 deb http://mirror.bit.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
 deb http://mirror.bit.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
@@ -35,7 +35,7 @@ deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-security main restricted univers
 deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
 deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
 ##测试版源
-deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse" >> sources.list
+deb-src http://mirror.bit.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse' > sources.list"
 sudo apt-get update
 
 }
